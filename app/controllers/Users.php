197aaa5792af468401d1f880caf308b4353529d5
DIrachid <rachidox.ochella@gmail.com>
2024-12-22 23:43:37 +0100

    you are stop in model user

---
+++ app/controllers/Users.php
@@ -0,0 +1,53 @@
+<?php
+
+#[AllowDynamicProperties]
+class Users extends Controller{
+
+    public function __construct(){
+        $this->usermodel = $this->model('User');
+    }
+
+    public function register(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+
+            $data = [
+                'name' => $_POST['username'],
+                'email' => $_POST['email'],
+                'password' => $_POST['password'],
+                'confirm_password' => $_POST['confirm_password'],
+                'name-err' => '',
+                'email-err' => '',
+                'password-err' => '',
+                'confirm-password-err' => '',
+            ];
+
+            if(empty($data['name'])) $data['name-err'] = 'Please fill your name';
+            if(empty($data['email'])) $data['email-err'] = 'Please fill your email';
+            if(empty($data['password'])) $data['password-err'] = 'Please fill your password';
+            if(empty($data['confirm_password'])) $data['confirm-password-err'] = 'Please fill your confirm password';
+            if($data['password'] !== $data['confirm_password']) $data['confirm-password-err'] = "password is don't match";
+
+            // check if you email exist
+
+            if($this->usermodel->getUserByEmail($data['email'])){
+                $data['email-err'] = 'email already taken';
+            }
+
+        }else{
+            $data = [
+                'name' => '',
+                'email' => '',
+                'password' => '',
+                'confirm-password' => '',
+                'name-err' => '',
+                'email-err' => '',
+                'password-err' => '',
+                'confirm-password-err' => '',
+            ];
+
+            // load the register
+            $this->view('users/register');
+        }
+    }
+}
