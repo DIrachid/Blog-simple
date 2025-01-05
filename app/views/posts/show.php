31ab72da307b483c37c0588df22ef40db8e4c04a
DIrachid <rachidox.ochella@gmail.com>
2025-01-05 17:16:56 +0100

    that sor anyone can possible see the post

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -6,8 +6,11 @@
 <div class="border rounded p-3 m-2">
     <?php echo $data->content; ?>
 </div>
-<?php if($data->user_id == $_SESSION['user_id']): ?>
+<?php if(isset($_SESSION['user_id'])): ?>
+<?php if($_SESSION['user_id'] == $data->user_id): ?>
+    <?php var_dump($_SESSION['user_id']) ?>
     <a href="<?php echo URLROOT ?>/posts/edit/<?php echo $data->id ?>" class="btn btn-info">Edit</a>
     <a href="<?php echo URLROOT ?>/posts/delete/<?php echo $data->id ?>" class="btn btn-danger">Delete</a>
+<?php endif; ?>
 <?php endif; ?>
 <?php include_once APPROOT . '/views/inc/footer.php'; ?>
